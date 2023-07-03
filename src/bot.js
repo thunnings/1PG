1213c0ad22d1f7c3e127b624a845cdeeb8653871
Travis Hunnings <travis.hunnings@gmail.com>
2023-07-02 22:36:01 -0400

    Implemented Event Handling

--- src/bot.js
+++ src/bot.js
@@ -1,18 +1,17 @@
+import { config } from 'dotenv';
+config({ path: '.env' });
+
 import { Client, GatewayIntentBits } from 'discord.js';
+import { EventHandler } from './handlers/event-handler.js';
 
 
-const bot = new Client({ 
+export const bot = new Client({ 
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages
     ]
 });
 
-bot.on('ready', () => console.log(bot.user.username + ' is online'));
-bot.on('messageCreate', async (msg) => {
-    if (msg.author.bot) return;
+new EventHandler().init();
 
-    await msg.reply('Hi');
-});
-
-bot.login('MTEyNDUzMzg3MjM0NDk3NzQyOA.GtY4Zi.4PpDHkj4v1Tk59p41OFEIuTBbT17rT2vSF-7P0');
+bot.login(process.env.BOT_TOKEN);
